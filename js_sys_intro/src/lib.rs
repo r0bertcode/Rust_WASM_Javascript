8701fb243d48e22e95cc05d160cd2bb80d9193a9
Rob <robertlopezca@gmail.com>
2021-05-13 22:13:14 -0700

    removed unesc return on first

--- js_sys_intro/src/lib.rs
+++ js_sys_intro/src/lib.rs
@@ -2,10 +2,6 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn first(array: js_sys::Array, n: u32) -> js_sys::Array {
-    if n == 0 {
-        return js_sys::Array::new();
-    }
-
     array.slice(0, n)
 }
 
