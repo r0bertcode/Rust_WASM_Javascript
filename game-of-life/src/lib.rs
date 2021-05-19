8d71480c519d180cf8d8affc3b08cc74e00f5d25
Rob <robertlopezca@gmail.com>
2021-05-18 21:08:44 -0700

    changed universe dimensions

--- game-of-life/src/lib.rs
+++ game-of-life/src/lib.rs
@@ -80,9 +80,13 @@ impl Universe {
 
                 // based on live_neighors count, we determine the cells fate
                 let next_cell = match (cell, live_neighbors) {
+                    // 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
                     (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // 2. Any live cell with two or three live neighbours lives on to the next generation.
                     (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
                     (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                     (Cell::Dead, 3) => Cell::Alive,
                     (otherwise, _) => otherwise,
                 };
@@ -99,7 +103,7 @@ impl Universe {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Universe {
         // Define height , width of our universe ( Cells x Cells )
-        let width = 306;
+        let width = 466;
         let height = 306;
 
         let cells = (0..width * height)
